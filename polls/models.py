f679983148d31fd58ffd3f4e777003427ed56f44
Halt07 <brychr@gmail.com>
2017-03-28 19:58:10 -0600

    fix question_text typo

--- polls/models.py
+++ polls/models.py
@@ -6,7 +6,7 @@ class Question(models.Model):
   question_text = models.CharField(max_length=200)
   pub_date = models.DateTimeField('date published')
   def __str__(self):
-    return self.qustion_text
+    return self.question_text
   
 class Choice(models.Model):
   question = models.ForeignKey(Question, on_delete=models.CASCADE)
