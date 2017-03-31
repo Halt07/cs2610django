d0c2427b072b55e0123b239c19dc07a88d7cbe73
Bryan Christensen <brychr@gmail.com>
2017-03-31 18:07:57 +0000

    pagecount migrations

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponse
-from .models import Question PageCount
+from .models import Question, PageCount
 
 def index(request):
-  ro, create = PageCount.objects.get_or_create(page='index')
+  row, create = PageCount.objects.get_or_create(page='index')
   row.count += 1
   row.save()
   latest_question_list = Question.objects.order_by('-pub_date')[:5]
