47aad02e9c2c51486f52fc96fee856239602cde6
Bryan Christensen <brychr@gmail.com>
2017-04-02 03:21:34 +0000

    add pagecounter to main index

--- polls/views.py
+++ polls/views.py
@@ -1,13 +1,19 @@
 from django.http import HttpResponse
+from django.template import loader
+
 from .models import Question, PageCount
 
 def index(request):
   row, create = PageCount.objects.get_or_create(page='index')
   row.count += 1
   row.save()
-  latest_question_list = Question.objects.order_by('-pub_date')[:5]
-  output = ', '.join([q.question_text for q in latest_question_list])
-  return HttpResponse(output)
+  latest_question_list = Question.objects.order_by('pub_date')[:5]
+  template = loader.get_template('polls/index.html')
+  context = {
+    'latest_question_list': latest_question_list,
+    'pagecount': str(row.count),
+    }
+  return HttpResponse(template.render(context,request))
 
 def detail(request, question_id):
   return HttpResponse("You're looking at question %s." % question_id)
