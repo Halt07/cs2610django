4d7c89d249c58306230c54082b04347826c57636
Bryan Christensen <brychr@gmail.com>
2017-04-07 21:40:03 +0000

    choice and results commit

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Question, PageCount
+from .models import Question, Choice, PageCount
 
 admin.site.register(Question)
+admin.site.register(Choice)
 admin.site.register(PageCount)
